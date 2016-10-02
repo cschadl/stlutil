0affd246ee56df5c5688ad4182e0a86c476b21ad
Chris Schadl <cschadl@gmail.com>
2016-10-02 12:54:04 -0500

    comment

--- move_accumulate.h
+++ move_accumulate.h
@@ -6,6 +6,9 @@
 namespace stlutil
 {
 
+/// Like std::accumulate, but aware of move semantics.
+/// accumulating into init when T has a move assignment
+/// and move constructor will result in no copies.
 template <class InputIt, class T, class BinaryOp>
 T&& move_accumulate(InputIt begin, InputIt end, T&& init, BinaryOp op)
 {
